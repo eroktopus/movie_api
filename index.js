6d10ccdd7f3e47c095bc94a78f41dff0f23d4427
eroktopus <147647841+eroktopus@users.noreply.github.com>
2024-03-04 18:37:10 -0500

    syntax fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const User = Models.User;
 
 const app = express();
 
-mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // mongoose.connect('mongodb://localhost:27017/mfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
