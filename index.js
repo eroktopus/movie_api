209ee384e1b4bbd52265b1c8f6886269f8a191cc
eroktopus <147647841+eroktopus@users.noreply.github.com>
2024-03-10 09:49:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:3000', 'http://testsite.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
