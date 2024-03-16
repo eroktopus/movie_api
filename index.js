d89b83e40ef6cf8c89f11b92c1404ee99e54b530
eroktopus <147647841+eroktopus@users.noreply.github.com>
2024-03-16 13:16:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -50,14 +50,14 @@ app.post('/users',
       return res.status(422).json({ errors: errors.array() });
     }
 
-    let hashedPassword = Users.hashPassword(req.body.Password);
-    await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    let hashedPassword = User.hashPassword(req.body.Password);
+    await User.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
       .then((user) => {
         if (user) {
           //If the user is found, send a response that it already exists
           return res.status(400).send(req.body.Username + ' already exists');
         } else {
-          Users
+          User
             .create({
               Username: req.body.Username,
               Password: hashedPassword,
