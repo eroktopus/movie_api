5d4f4ca5debff4657b055b9e4a0bdb4c67fcd1b9
eroktopus <147647841+eroktopus@users.noreply.github.com>
2024-03-20 19:29:01 -0400

    Update models.js

--- models.js
+++ models.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const movieSchema = new mongoose.Schema({
   Title: { type: String, required: true },
   Description: { type: String, required: true },
-  Genre: [{ type: Schema.Types.ObjectId, ref: 'Genre', required: true }],
+  Genre: { type: String, required: true },
   Director: {
       Name: String,
       Bio: String
