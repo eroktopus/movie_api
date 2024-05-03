01eba4dacbc261dedcc145165948d19162b77958
eroktopus <147647841+eroktopus@users.noreply.github.com>
2024-05-03 13:05:32 -0400

    tweaks

--- models.js
+++ models.js
@@ -1,49 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 const movieSchema = new mongoose.Schema({
   Title: { type: String, required: true },
   Description: { type: String, required: true },
-  Genre: { type: String, required: true },
+  Genre: { name: String, description: String },
   Director: {
-      Name: String,
-      Bio: String
+    Name: String,
+    Bio: String,
   },
   Actors: [String],
   ImagePath: String,
-  Featured: Boolean
+  Featured: Boolean,
 });
 
-
-
 let userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
   Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
 
 userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = function(password) {
+userSchema.methods.validatePassword = function (password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
-const genreSchema = new Schema({
-  Name: { type: String, required: true }
-}, { collection: 'genres' });
+const genreSchema = new Schema(
+  {
+    Name: { type: String, required: true },
+  },
+  { collection: "genres" }
+);
 
-const directorSchema = new Schema({
-  Name: { type: String, required: true }
-}, { collection: 'directors' });
+const directorSchema = new Schema(
+  {
+    Name: { type: String, required: true },
+  },
+  { collection: "directors" }
+);
 
-const Movie = mongoose.model('Movie', movieSchema);
-const User = mongoose.model('User', userSchema);
-const Genre = mongoose.model('Genre', genreSchema);
-const Director = mongoose.model('Director', directorSchema); // Fixed model name
+const Movie = mongoose.model("Movie", movieSchema);
+const User = mongoose.model("User", userSchema);
+const Genre = mongoose.model("Genre", genreSchema);
+const Director = mongoose.model("Director", directorSchema); // Fixed model name
 
 module.exports = { Movie, User, Genre, Director };
